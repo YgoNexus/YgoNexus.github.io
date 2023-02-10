4174fc2b9343a98b0fa3609bb998669db38a9b88
ygonexus <ygonexus0@gmail.com>
2023-02-11 00:03:44 +0800

    Site updated: 2023-02-11 00:03:42

--- dist/music.js
+++ dist/music.js
@@ -3,54 +3,61 @@ const ap = new APlayer({
     fixed: true,
     autoplay: true,
     theme: '#b7daff',
-    loop: 'all',
-    order: 'list',
+    loop: 'all', // one /none
+    order: 'list',	// random
     preload: 'auto',
-    volume: 0.7,
+    volume: 0.6,
     mutex: true,
     listFolded: false,
-    listMaxHeight: '200px',
-    lrcType: 2,
+    listMaxHeight: '300px',
+    lrcType: 1,
 	// 3是使用audio.lrc   1是JS  2是HTML
     audio: [
       {
         name: "敢归云间宿",
         artist: '三无',
         url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
-        cover: '/music/cover/敢归云间宿.jpg',
-        lrc: '/music/lyrics/敢归云间宿.lrc',
+        cover: '/music/cover/敢归云间宿.png',
+        // lrc: '/music/lyrics/敢归云间宿.lrc',
       },
       {
-        name: '哥哥爸爸真偉大',
-        artist: '兒歌',
-        url: '/music/飞鸟和蝉.mp3',
-        cover: '/music/cover/哥哥爸爸真偉大.jpg',
-        lrc: '/music/lyrics/哥哥爸爸真偉大.lrc'
-      }
+        name: '老教室',
+        artist: '831',
+        url: '/music/老教室.flac',
+        cover: '/music/cover/831_大逃杀.jpg',
+        // lrc: '/music/lyrics/老教室.lrc',
+      },
+      {
+        name: "敢归云间宿",
+        artist: '三无',
+        url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
+        cover: '/music/cover/敢归云间宿.png',
+        // lrc: '/music/lyrics/敢归云间宿.lrc',
+      },
     ]
 });
 
-
-const colorThief = new ColorThief();
-const image = new Image();
-const xhr = new XMLHttpRequest();
-const setTheme = (index) => {
-    if (!ap.list.audios[index].theme) {
-        xhr.onload = function(){
-            let coverUrl = URL.createObjectURL(this.response);
-            image.onload = function(){
-                let color = colorThief.getColor(image);
-                ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
-                URL.revokeObjectURL(coverUrl)
-            };
-            image.src = coverUrl;
-        }
-        xhr.open('GET', ap.list.audios[index].cover, true);
-        xhr.responseType = 'blob';
-        xhr.send();
-    }
-};
-setTheme(ap.list.index);
-ap.on('listswitch', (index) => {
-    setTheme(index);
-});
+// const colorThief = new ColorThief();
+// const image = new Image();
+// const xhr = new XMLHttpRequest();
+// const setTheme = (index) => {
+    // if (!ap.list.audios[index].theme) {
+        // xhr.onload = function(){
+            // let coverUrl = URL.createObjectURL(this.response);
+            // image.onload = function(){
+                // let color = colorThief.getColor(image);
+				// window.alert(color);
+                // ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
+                // URL.revokeObjectURL(coverUrl)
+            // };
+            // image.src = coverUrl;
+        // }
+        // xhr.open('GET', ap.list.audios[index].cover, true);
+        // xhr.responseType = 'blob';
+        // xhr.send();
+    // }
+// };
+// setTheme(ap.list.index);
+// ap.on('listswitch', (index) => {
+    // setTheme(index);
+// });
