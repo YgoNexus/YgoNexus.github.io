f74c129907b43a655c0607b7944c47e7eecfd2f1
ygonexus <ygonexus0@gmail.com>
2023-02-15 00:18:48 +0800

    Site updated: 2023-02-15 00:18:06

--- dist/music.js
+++ dist/music.js
@@ -1,42 +1,47 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: true,
-    theme: '#b7daff',
-    loop: 'all', // one /none
-    order: 'list',	// random
-    preload: 'auto',
-    volume: 0.6,
-    mutex: true,
-    listFolded: false,
-    listMaxHeight: '300px',
-    lrcType: 1,
-	// 3是使用audio.lrc   1是JS  2是HTML
-    audio: [
-      {
-        name: "敢归云间宿",
-        artist: '三无',
-        url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
-        cover: '/music/cover/敢归云间宿.png',
-        // lrc: '/music/lyrics/敢归云间宿.lrc',
-      },
-      {
-        name: '老教室',
-        artist: '831',
-        url: '/music/老教室.flac',
-        cover: '/music/cover/831_大逃杀.jpg',
-        // lrc: '/music/lyrics/老教室.lrc',
-      },
-      {
-        name: "敢归云间宿",
-        artist: '三无',
-        url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
-        cover: '/music/cover/敢归云间宿.png',
-        // lrc: '/music/lyrics/敢归云间宿.lrc',
-      },
-    ]
-});
 
+var initialized = false;
+if(!initialized)
+{
+	initialized = true;
+	const ap = new APlayer({
+		container: document.getElementById('aplayer'),
+		fixed: true,
+		autoplay: true,
+		theme: '#b7daff',
+		loop: 'all', // one /none
+		order: 'list',	// random
+		preload: 'auto',
+		volume: 0.6,
+		mutex: true,
+		listFolded: false,
+		listMaxHeight: '300px',
+		lrcType: 1,
+		// 3是使用audio.lrc   1是JS  2是HTML
+		audio: [
+		  {
+			name: "敢归云间宿",
+			artist: '三无',
+			url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
+			cover: '/music/cover/敢归云间宿.png',
+			// lrc: '/music/lyrics/敢归云间宿.lrc',
+		  },
+		  {
+			name: '老教室',
+			artist: '831',
+			url: '/music/老教室.flac',
+			cover: '/music/cover/831_大逃杀.jpg',
+			// lrc: '/music/lyrics/老教室.lrc',
+		  },
+		  {
+			name: "敢归云间宿",
+			artist: '三无',
+			url: 'https://drive.google.com/uc?export=download&id=1u7dE6gzMjclBb0_B2uln50AMIdVEIuVe',
+			cover: '/music/cover/敢归云间宿.png',
+			// lrc: '/music/lyrics/敢归云间宿.lrc',
+		  },
+		]
+	});
+}
 // const colorThief = new ColorThief();
 // const image = new Image();
 // const xhr = new XMLHttpRequest();
